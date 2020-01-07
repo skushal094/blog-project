28f2dbc65d8f57cb9a1beb7523ab75eb639c9009
Kushal Shah <skushal094@gmail.com>
2020-01-07 19:01:36 +0530

    Migrations for Blog, Like and Comment have been completed.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField(max_length=350)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=True)
+    is_deleted = models.BooleanField(default=False)
+
+
+class Like(models.Model):
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=True)
+    is_deleted = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=True)
+    is_deleted = models.BooleanField(default=False)
